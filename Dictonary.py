0a9bd4ff1a31db147ae8d2208d66b513d79ab553
Aguayo <gjaguayo@cps.k12.il.us>
2024-09-30 11:56:46 -0500

    Dictonary

---
+++ Dictonary.py
@@ -0,0 +1,36 @@
+#dicionary = a coillection of {key:value} pairs orderd and changeable. No duplicates 
+
+capitals = {"USA" : " Washington D.C",
+            "India" : "New Delhi",
+            "China" : "Beijing",
+            "Russia" : "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+
+# print(capitals.get("USA"))
+
+# if capitals.get("Russia"):
+#     print("That Capital Exsisits")
+# else:
+#     print("That capital dosent exsist")
+
+# # #Update capitals 
+# capitals.update({"Germany": "Berlin"})
+# capitals.update({"USA": "Detroit"})
+
+# capitals.pop("China")
+# capitals.popitem()
+# capitals,clear()
+
+# keys= capitals.keys()
+# # for key in capitals.keys()
+# # print(keys)
+
+# #values = capitals.values()
+# #for value in capitals.values():
+# # pirnt(value)
+
+# #items = capitals.items()
+# for key, value in capitals.items():
+#     print{f"(key) : (valu)"}
